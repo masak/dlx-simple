6948c14e7d20b07bf77d3924149fd761a748f0ab
Carl Masak <cmasak@gmail.com>
2011-08-07 01:08:01 +0200

    [Writer::Rowset] actually fleshes out this class
    Haven't needed this class until now.

--- perl5/lib/Writer.pm
+++ perl5/lib/Writer.pm
@@ -1,4 +1,10 @@
 package Writer;
 use Moose;
 
+sub write {
+    my ($self) = @_;
+
+    die "Class ", ref($self), " doesn't override abstract method 'write'.";
+}
+
 1;
--- perl5/lib/Writer/Rowset.pm
+++ perl5/lib/Writer/Rowset.pm
@@ -1,1 +1,18 @@
+use 5.010;
+use strict;
+use warnings;
+
+package Writer::Rowset;
+
+use Moose;
+
+extends 'Writer';
+
+sub write {
+    my ($self, $rowset) = @_;
+
+    say sprintf "[%s]", join ", ", @{$rowset};
+    return;
+}
+
 1;
