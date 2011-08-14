d9a670fd23ce402421d3ac81e22a2269448a04cc
Carl Masak <cmasak@gmail.com>
2011-08-14 09:51:36 +0200

    introduced linked list representation

--- c/src/test.c
+++ c/src/test.c
@@ -41,6 +41,21 @@ typedef struct {
     struct rows_stack_element* top;
 } rows_stack;
 
+struct data_object {
+    struct data_object *L, *R, *U, *D;
+};
+
+struct data_header {
+    struct data_object data_object;
+    int S;
+};
+
+struct data_node {
+    struct data_object data_object;
+    struct data_header *C;
+    int row;
+};
+
 dlx_matrix *create_dlx_matrix() {
     dlx_matrix *new_matrix;
 
@@ -246,6 +261,17 @@ int width(dlx_matrix *matrix) {
     return max + 1;
 }
 
+int height(dlx_matrix *matrix) {
+    int h = 0;
+    struct dlx_matrix_row *row = matrix->first_row;
+
+    while (row != NULL) {
+        h++;
+        row = row->next_row;
+    }
+    return h;
+}
+
 char *contents(struct solution_matrix_row *row) {
     char *contents = malloc(50 * sizeof (char));
     struct solution_matrix_value *value = row->first_value;
@@ -351,34 +377,172 @@ void destroy_rows_stack(rows_stack *stack) {
     free(stack);
 }
 
-void search_dancing_links(dlx_matrix *dlx_matrix, solution_matrix *solutions,
-                          int column, rows_stack *rows_stack) {
-    if (column >= width(dlx_matrix)) {
+struct data_header *create_data_header() {
+    struct data_header *new_data_header = malloc(sizeof (struct data_header));
+    struct data_object *new_data_header_do = (struct data_object *)new_data_header;
+    new_data_header_do->L = new_data_header_do;
+    new_data_header_do->R = new_data_header_do;
+    new_data_header_do->U = new_data_header_do;
+    new_data_header_do->D = new_data_header_do;
+    new_data_header->S = 0;
+    return new_data_header;
+}
+
+void increase_S(struct data_header *header) {
+    header->S++;
+}
+
+void decrease_S(struct data_header *header) {
+    header->S--;
+}
+
+struct data_node *create_data_node(int row, struct data_header *header) {
+    struct data_node *new_data_node = malloc(sizeof (struct data_node));
+    struct data_object *new_data_node_do = (struct data_object *)new_data_node;
+    new_data_node_do->L = new_data_node_do;
+    new_data_node_do->R = new_data_node_do;
+    new_data_node_do->U = new_data_node_do;
+    new_data_node_do->D = new_data_node_do;
+    new_data_node->row = row;
+    new_data_node->C = header;
+    increase_S(header);
+    return new_data_node;
+}
+
+void attach_below(struct data_object *self, struct data_object *other) {
+    other->D->U = self;
+    self->D = other->D;
+    self->U = other;
+    other->D = self;
+}
+
+void attach_to_right_of(struct data_object *self, struct data_object *other) {
+    other->R->L = self;
+    self->R = other->R;
+    self->L = other;
+    other->R = self;
+}
+
+void attach_to_left_of(struct data_object *self, struct data_object *other) {
+    attach_to_right_of(other, self);
+}
+
+struct data_header *linked_representation(dlx_matrix *matrix) {
+    int w = width(matrix);
+    int h = height(matrix);
+    struct data_header *root = create_data_header();
+    struct data_header *last_seen_header = root;
+    struct data_node **last_seen_on_row
+        = calloc(h, sizeof(struct data_node*));
+    int i;
+
+    for (i = 0; i < w; i++) {
+        struct data_header *header = create_data_header();
+        struct data_object *last_seen_node = (struct data_object *)header;
+        struct dlx_matrix_row *current_row;
+        int j = 0;
+        attach_to_right_of((struct data_object *)header,
+                           (struct data_object*)last_seen_header);
+        last_seen_header = header;
+
+        current_row = matrix->first_row;
+        while (current_row != NULL) {
+            if (dlx_row_contains_value(current_row, i)) {
+                struct data_node *data_node = create_data_node(j, header);
+                attach_below((struct data_object *)data_node, last_seen_node);
+                last_seen_node = (struct data_object *)data_node;
+
+                if (last_seen_on_row[j] != NULL) {
+                    attach_to_right_of(
+                        (struct data_object *)data_node,
+                        (struct data_object *)last_seen_on_row[j]
+                    );
+                }
+                last_seen_on_row[j] = data_node;
+            }
+
+            current_row = current_row->next_row;
+            j++;
+        }
+    }
+    free(last_seen_on_row);
+    return root;
+}
+
+struct data_header *choose_column(struct data_header *root) {
+    return (struct data_header *)((struct data_object *)root)->R;
+}
+
+void cover_column(struct data_header *c) {
+    struct data_object *i, *j;
+    struct data_object *cdo = (struct data_object *)c;
+    cdo->R->L = cdo->L;
+    cdo->L->R = cdo->R;
+    for (i = cdo->D; i != cdo; i = i->D) {
+        for (j = i->R; j != i; j = j->R) {
+            j->D->U = j->U;
+            j->U->D = j->D;
+            decrease_S(((struct data_node *)j)->C);
+        }
+    }
+}
+
+void uncover_column(struct data_header *c) {
+    struct data_object *i, *j;
+    struct data_object *cdo = (struct data_object *)c;
+    for (i = cdo->U; i != cdo; i = i->U) {
+        for (j = i->L; j != i; j = j->L) {
+            increase_S(((struct data_node *)j)->C);
+            j->D->U = j;
+            j->U->D = j;
+        }
+    }
+    cdo->R->L = cdo;
+    cdo->L->R = cdo;
+}
+
+void solve_dancing_links_helper(
+    struct data_header *root,
+    rows_stack *stack,
+    solution_matrix *solutions) {
+
+    if (((struct data_object *)root)->R == (struct data_object *)root) {
         struct solution_matrix_row *solution = add_solution(solutions);
-        add_solution_values_from_stack(solution, rows_stack);
+        add_solution_values_from_stack(solution, stack);
     }
     else {
-        struct dlx_matrix_row *row = dlx_matrix->first_row;
-        int row_number = 0;
-        while (row != NULL) {
-            if (dlx_row_contains_value(row, column)) {
-                rows_push(rows_stack, row_number);
-                search_dancing_links(dlx_matrix, solutions, column + 1,
-                                     rows_stack);
-                rows_pop(rows_stack);
+        struct data_header *c = choose_column(root);
+        struct data_object *cdo = (struct data_object *)c;
+        struct data_object *r;
+        cover_column(c);
+        for (r = cdo->D; r != cdo; r = r->D) {
+            struct data_object *j;
+            rows_push(stack, ((struct data_node *)r)->row);
+            for (j = r->R; j != r; j = j->R) {
+                cover_column(((struct data_node *)j)->C);
             }
-            row_number++;
-            row = row->next_row;
+
+            solve_dancing_links_helper(root, stack, solutions);
+
+            for (j = r->L; j != r; j = j->L) {
+                uncover_column(((struct data_node *)j)->C);
+            }
+            rows_pop(stack);
         }
+        uncover_column(c);
     }
 }
 
 solution_matrix *solve_dancing_links(dlx_matrix *dlx_matrix) {
     solution_matrix *solutions = create_solution_matrix();
     rows_stack *stack = create_rows_stack();
+    struct data_header *root;
 
-    search_dancing_links(dlx_matrix, solutions, 0, stack);
+    root = linked_representation(dlx_matrix);
 
+    solve_dancing_links_helper(root, stack, solutions);
+
+    /* need to destroy the linked representation here */
     destroy_rows_stack(stack);
     return solutions;
 }
@@ -458,7 +622,7 @@ void test_that_2_by_2_candidates_gives_4_solutions() {
     add_solution_value(solution, 3);
 
     test_solve(two_by_two, four_solutions,
-               "identity matrix gives all lines");
+               "2x2 candidates gives 4 solutions");
 
     destroy_solution_matrix(four_solutions);
     destroy_dlx_matrix(two_by_two);
